d18a974d016623b322c8e1cae0b997bdc4e36923
Frankie <fdsimms@gmail.com>
2018-07-10 01:38:35 -0400

    fix some stuff

--- redirect.js
+++ redirect.js
@@ -1,2 +1,2 @@
 window.location = "https://www.nytimes.com";
-window.alert("It appears you just tried to access a New York Times Op-Ed. Be more careful, please.");
+window.alert("It appears you just tried to access the New York Times Op-Ed Section. Be more careful, please.");
